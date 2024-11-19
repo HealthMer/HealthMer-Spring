b310c27ae2f227d6eaf8d776c15bb8c1b3979c06
dylan-yoon <d.dylany21@gmail.com>
2024-11-19 13:28:27 +0900

    bulid: Spring Setting

---
+++ HealthMer/src/test/java/com/minijean/healthmer/HealthMerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minijean.healthmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthMerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
