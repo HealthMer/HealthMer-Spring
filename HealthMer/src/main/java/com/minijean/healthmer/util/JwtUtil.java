958af5d824680d44f0bad1b18825197516532b89
dylan-yoon <d.dylany21@gmail.com>
2024-11-25 17:36:03 +0900

    feat: access token 변경

--- HealthMer/src/main/java/com/minijean/healthmer/util/JwtUtil.java
+++ HealthMer/src/main/java/com/minijean/healthmer/util/JwtUtil.java
@@ -7,6 +7,8 @@ import javax.crypto.SecretKey;
 
 import org.springframework.stereotype.Component;
 
+import com.minijean.healthmer.model.dto.User;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
@@ -16,15 +18,27 @@ import io.jsonwebtoken.security.Keys;
 public class JwtUtil {
 	private String key = "SSAFY_NonMajor_JavaTrack_SecretKey";
 	private SecretKey secretKey = Keys.hmacShaKeyFor(key.getBytes(StandardCharsets.UTF_8));
-	
-	public String createToken(String name) {
-		Date exp = new Date(System.currentTimeMillis()+ 1000*60*60*6);
-		return Jwts.builder().header().add("typ", "JWT").and()
-				.claim("name", name).expiration(exp)
-				.signWith(secretKey).compact();
+
+	public String createTokenBy(User user) {
+		Date exp = new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 6);
+		return Jwts.builder().header().add("typ", "JWT").and().claim("id", user.getId()).claim("email", user.getEmail())
+				.expiration(exp).signWith(secretKey).compact();
 	}
-	
-	public Jws<Claims> vaildate(String token){
+
+//	public String createLoginToken(String name) {
+//		Date exp = new Date(System.currentTimeMillis()+ 1000*60*60*6);
+//		return Jwts.builder().header().add("typ", "JWT").and()
+//				.claim("name", name).expiration(exp)
+//				.signWith(secretKey).compact();
+//	}
+
+	public Jws<Claims> validate(String token) {
 		return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token);
 	}
+
+    public String extractUserId(String token) {
+    	// getBody() 메서드가 deprecated된 경우 대안은 라이브러리의 최신 문서를 참조하여 새로운 API로 업데이트된 방식에 맞게 코드를 작성하는 것입니다. 하지만, 현재의 최신 JJWT 라이브러리에서는 위의 방식이 유효하고 권장되는 패턴입니다.
+        Claims claims = validate(token).getBody(); // 클레임을 추출하는 권장되는 방법 사용
+        return claims.get("id", String.class); // "id" 클레임에서 사용자 ID 추출
+    }
 }
